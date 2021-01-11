e18d57ec168401f526c4c458a2b03d330668aa2f
Mai Pham <maitphm@gmail.com>
2021-01-10 20:14:42 -0500

    commit tree b65bf472af2ff630ca00d519ef6bef9344abb30c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const arrOfNames = (["Lisa", "Kaitlin", "Jan"]);
+const event = "birthday"
+
+function writeCards(arrOfNames, event) { 
+    let greetingArrays= [] 
+    for (let i = 0; i < arrOfNames.length; i++) {
+        greetingArrays.push(`Thank you, ${arrOfNames[i]}, for the wonderful ${event} gift!`)
+    }
+    return greetingArrays;
+}
+
+function countDown(i) { 
+    // let i = number; 
+    while (i>= 0) {
+        console.log(i--);
+    }
+}
